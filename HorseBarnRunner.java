d6d88fa2dc250509fc64ca1351f7a0d1f3a6c3f7
nakulsoneji <nakulsoneji@gmail.com>
2024-02-06 02:05:46 -0800

    fixes errors

--- HorseBarnRunner.java
+++ HorseBarnRunner.java
@@ -8,8 +8,7 @@ public class HorseBarnRunner {
   public static void main(String[] args) {
     HorseBarn barn = new HorseBarn();
     ArrayList<Horse> barnSpaces = barn.getSpaces();
-    int numSpaces = barnSpaces.size();
-    for (int i = 0; i < numSpaces; i++) {
+    for (int i = 0; i < barnSpaces.size(); i++) {
       Horse h = barnSpaces.get(i);
       if (h.getName().equals("Patches"))
         System.out.println("Bye bye " + barnSpaces.remove(i));
